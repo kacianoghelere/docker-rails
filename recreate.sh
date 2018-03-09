da3e2d7a9e486f0d12a43b3fd9d0c769081a0684
Kaciano Ghelere <themohawkeagle@gmail.com>
2018-03-08 23:54:27 -0300

    Alguns executaveis adicionados

--- recreate.sh
+++ recreate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Reconstruindo base de dados";
-sudo docker-compose run web rake db:drop db:create db:migrate db:seed db:schema:dump
+sudo docker-compose run rails rake db:drop db:create db:migrate db:seed db:schema:dump
